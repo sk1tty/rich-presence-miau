79d7af683171b133d30453151c2fd4ffef340fa6
Isabella <72838329+sk1tty@users.noreply.github.com>
2021-08-07 16:42:11 -0500

    Update miau.js

--- Rich Presence de Isa/miau.js
+++ Rich Presence de Isa/miau.js
@@ -5,17 +5,17 @@ client.on('ready', () => {
   client.request('SET_ACTIVITY', {
     pid: process.pid,
     activity : {
-      details : "alianzas md 3k", //acá puedes escribir lo que quieras :3
+      details : "vivan los mishis putos", //acá puedes escribir lo que quieras :3
       assets : {
-        large_image : "05f9a", //el código de una de las imágenes de tu app 
+        large_image : "05f9a", //el código de una de las imágenes de tu app (el nombre) 
         large_text : "miau"  // texto que aparece en la imagen 
          
       },
-      buttons : [{label : "discord" , url : "https://discord.gg/97uKN2N2hQ"}] // uwu
+      buttons : [{label : "discord server" , url : "https://discord.gg/mishisquad"}] // acá puedes poner la url de una red social!
       
     }
   });
   
 });
 
-client.login({ clientId : "494769648445947915" }).catch(console.error);  // en donde dice client ID, pones la id de tu app
+client.login({ clientId : "494769648445947915" }).catch(console.error);  // en donde dice client ID, pones la id de tu app que hiciste en el developer portal 
